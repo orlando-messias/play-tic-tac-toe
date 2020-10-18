e3a37d0eb86917fa507402c0e3a0c45fe04ccf85
orlando-messias <programadorlando@gmail.com>
2020-10-18 17:09:15 -0300

    runScript.sh

---
+++ server/runScripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "--------------------------"
+echo "Starting api Tic Tac Toe"
+echo ""
+echo "Installing dependencies..."
+echo ""
+npm install
+echo ""
+echo "Starting application"
+echo "--------------------------"
+npm start
